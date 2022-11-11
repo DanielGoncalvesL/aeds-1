4dc5444627091739e4db6b3059bd8e81940f14ad
JosineyJr <josineyjunior14@gmail.com>
2022-11-11 17:24:46 -0300

    24

---
+++ 24.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Josiney Junior on 11/11/22.
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int limiteInferior, limiteSuperior, valorNoIntervalo;
+
+    cout << "Informe o valor do limite inferior: ";
+    cin >> limiteInferior;
+
+    cout << "Informe o valor do limite superior: ";
+    cin >> limiteSuperior;
+
+    if (limiteInferior % 2 == 0)
+        limiteInferior += 2;
+    else
+        limiteInferior += 1;
+
+    float somatorio = 0;
+    for (valorNoIntervalo = limiteInferior; valorNoIntervalo <= limiteSuperior - 1; valorNoIntervalo += 2)
+    {
+        somatorio += valorNoIntervalo;
+    }
+
+    cout << "Somatório: " << somatorio;
+
+    return 0;
+}
