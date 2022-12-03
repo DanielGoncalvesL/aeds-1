05332b6ceb196a1624895b292495fec7b9c0132d
JosineyJr <josineyjunior14@gmail.com>
2022-12-03 19:07:26 -0300

    feat: lista02 exercicio 8

---
+++ Lista02/08.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float numeros[3] = {};
+
+    srand((unsigned int)time(NULL));
+
+    int menor;
+    for (int i = 0; i < 3; i++)
+    {
+        numeros[i] = (float(rand()) / float(RAND_MAX)) * (rand() % 100);
+        cout << numeros[i] << endl;
+
+        if (i > 0 && numeros[i] < numeros[i - 1])
+        {
+            menor = i;
+        }
+    }
+
+    float somaMaiores = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        if (i == menor)
+            continue;
+
+        somaMaiores += numeros[i];
+    }
+
+    cout << "Menor: " << menor << endl;
+    cout << "Soma dos maiores: " << somaMaiores << endl;
+
+    return 0;
+}
